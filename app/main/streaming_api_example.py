7317c57be052cc7883aeaafda73e22cb6e8228c0
Shayan Patel <shayanpatel@levyvlan502.0784.wlan.wireless-resnet.upenn.edu>
2015-12-08 17:15:10 -0500

    changing the api example

--- app/main/streaming_api_example.py
+++ app/main/streaming_api_example.py
@@ -13,19 +13,28 @@ import time
 # 		token_secret=ACCESS_TOKEN_SECRET
 # 	)
 
-# t = Twitter(auth=o_auth)
 
-# twitter_stream = TwitterStream(auth=o_auth, domain='userstream.twitter.com')
-
-# for msg in twitter_stream.user():
-# 	print(msg)
 
 
 def run(d):
-	while True:
+	CONSUMER_KEY = d['keys']['CONSUMER_KEY']
+	CONSUMER_SECRET = "kpdLGCtnUnMRt80IBbrosAUwfMCqZ5P7K6OZNGSxcmSLj5TdSw"
+	ACCESS_TOKEN = "4266132851-zMFB5Uody378iP1tMBMr76oUQsFSQ4BNm0VtKOl"
+	ACCESS_TOKEN_SECRET = "yTPssEILIbGvJ8KZeXzVxItLDPGaFZ3hF8nRMR9Y73TQI"
+
+	o_auth = OAuth(
+		consumer_key=CONSUMER_KEY,
+		consumer_secret=CONSUMER_SECRET,
+		token=ACCESS_TOKEN,
+		token_secret=ACCESS_TOKEN_SECRET
+	)
+
+	t = Twitter(auth=o_auth)
+
+	twitter_stream = TwitterStream(auth=o_auth, domain='userstream.twitter.com')
+
+	for msg in twitter_stream.user():
 		if not d['running']:
 			break
-		else:
-			d['counter'] += 1
-			print('Process: incrementing counter')
-			time.sleep(1)
+
+
